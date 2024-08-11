8d53c653b0bff0d314786c967c9f5f5a3efab4fd
MrKeith99 <kvalentincardenas@hotmail.com>
2024-08-11 13:49:59 +0900

    [ADD] new installer (tbc)

---
+++ install.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+echo "╔══╣ Setup: SOBIT LIGHT (STARTING) ╠══╗"
+
+
+# Keep track of the current directory
+DIR=`pwd`
+cd ..
+
+# Download required packages for SOBIT LIGHT
+ros_packages=(
+    "sobit_common" \
+    "sobits_msgs" \
+    # "urg_node" \
+    # "azure_kinect_ros_driver" \
+    "realsense_ros" \
+    "kachaka_api"
+)
+
+# Clone all packages
+for ((i = 0; i < ${#ros_packages[@]}; i++)) {
+    echo "Clonning: ${ros_packages[i]}"
+    git clone https://github.com/TeamSOBITS/${ros_packages[i]}.git
+
+    # Check if install.sh exists in each package
+    if [ -f ${ros_packages[i]}/install.sh ]; then
+        echo "Running install.sh in ${ros_packages[i]}."
+        cd ${ros_packages[i]}
+        bash install.sh
+        cd ..
+    fi
+}
+
+# Go back to previous directory
+cd ${DIR}
+
+# Download required dependencies
+# sudo apt-get update
+# sudo apt-get install -y \
+#     mpg321 
+
+# Download ROS packages
+sudo apt-get update
+sudo apt-get install -y \
+    ros-$ROS_DISTRO-control-toolbox \
+    ros-$ROS_DISTRO-controller-interface \
+    ros-$ROS_DISTRO-controller-manager \
+    ros-$ROS_DISTRO-position-controllers \
+    ros-$ROS_DISTRO-velocity-controllers \
+    ros-$ROS_DISTRO-effort-controllers \
+    ros-$ROS_DISTRO-joint-trajectory-controller \
+    ros-$ROS_DISTRO-joint-group-impedance-controller \
+    ros-$ROS_DISTRO-joint-state-publisher \
+    ros-$ROS_DISTRO-joint-state-publisher-gui \
+    ros-$ROS_DISTRO-joint-state-broadcaster \
+    ros-$ROS_DISTRO-joint-limits-interface \
+    ros-$ROS_DISTRO-robot-controllers \
+    ros-$ROS_DISTRO-robot-controllers-interface \
+    ros-$ROS_DISTRO-robot-state-publisher \
+    ros-$ROS_DISTRO-hardware-interface \
+    ros-$ROS_DISTRO-transmission-interface \
+    ros-$ROS_DISTRO-urdf \
+    ros-$ROS_DISTRO-urdf-launch \
+    ros-$ROS_DISTRO-xacro \
+
+# Install Gazebo Fortress and ROS2 Control Gazebo Plugins
+sudo apt-get update
+
+cd ..
+git clone https://github.com/ros-controls/gz_ros2_control/ -b ${ROS_DISTRO};
+rosdep update
+rosdep install --from-paths gz_ros2_control/ -i -y --rosdistro ${ROS_DISTRO}
+
+
+# # Setting up Dynamixel USB configuration (SOBIT LIGHT: Head and Arm Robot Mechanism)
+# echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6015\", SYMLINK+=\"input/dx_upper\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/dx_upper.rules
+
+# # Setting up PS4 Joystick USB configuration
+# echo "KERNEL==\"uinput\", MODE=\"0666\"
+#       KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", ATTRS{idVendor}==\"054c\", ATTRS{idProduct}==\"05c4\", MODE=\"0666\"
+#       KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", KERNELS==\"0005:054C:05C4.*\", MODE=\"0666\"
+#       KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", ATTRS{idVendor}==\"054c\", ATTRS{idProduct}==\"09cc\", MODE=\"0666\"
+#       KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", KERNELS==\"0005:054C:09CC.*\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/50-ds4drv.rules
+
+# # Reload udev rules
+# sudo udevadm control --reload-rules
+
+# # Trigger the new rules
+# sudo udevadm trigger
+
+# Go back to previous directory
+cd ${DIR}
+
+
+echo "╚══╣ Setup: SOBIT LIGHT (FINISHED) ╠══╝"
