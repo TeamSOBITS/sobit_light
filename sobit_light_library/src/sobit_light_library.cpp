8a4593a58c03010f6de62fae4d3b9339e01ed755
MrKeith99 <kvalentincardenas@hotmail.com>
2024-08-18 16:07:37 +0900

    [FIX] joint lib values

--- sobit_light_library/src/sobit_light_library.cpp
+++ sobit_light_library/src/sobit_light_library.cpp
@@ -25,103 +25,119 @@ PYBIND11_MODULE(sobit_light_module, m) {
     .def(pybind11::init<const std::string&>())
     .def("moveToPose", &JointController::moveToPose, "move Pose",
         pybind11::arg("pose_name"),
-        pybind11::arg("sec") = 5.0)
-    .def("moveAllJointsRad", &JointController::moveAllJointsRad, "move all Joint",
-        pybind11::arg("arm_shoulder_pan"),
-        pybind11::arg("arm_shoulder_tilt"),
-        pybind11::arg("arm_elbow_tilt"),
-        pybind11::arg("arm_wrist_tilt"),
-        pybind11::arg("hand"),
-        pybind11::arg("head_camera_pan"),
-        pybind11::arg("head_camera_tilt"),
         pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
-    .def("moveAllJointsDeg", &JointController::moveAllJointsDeg, "move all Joint",
-        pybind11::arg("arm_shoulder_pan"),
-        pybind11::arg("arm_shoulder_tilt"),
-        pybind11::arg("arm_elbow_tilt"),
-        pybind11::arg("arm_wrist_tilt"),
+    .def("moveAllJointsDeg", &JointController::moveAllJointsDeg, "moveAllJointsDeg",
+        pybind11::arg("arm_shoulder_roll"),
+        pybind11::arg("arm_shoulder_pitch"),
+        pybind11::arg("arm_elbow_pitch"),
+        pybind11::arg("arm_forearm_roll"),
+        pybind11::arg("arm_wrist_pitch"),
+        pybind11::arg("arm_wrist_roll"),
         pybind11::arg("hand"),
-        pybind11::arg("head_camera_pan"),
-        pybind11::arg("head_camera_tilt"),
+        pybind11::arg("head_yaw"),
+        pybind11::arg("head_pitch"),
         pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
-    .def("moveJointRad", &JointController::moveJointRad, "moveJoint",
+    .def("moveAllJointsRad", &JointController::moveAllJointsRad, "moveAllJointsRad",
+        pybind11::arg("arm_shoulder_roll"),
+        pybind11::arg("arm_shoulder_pitch"),
+        pybind11::arg("arm_elbow_pitch"),
+        pybind11::arg("arm_forearm_roll"),
+        pybind11::arg("arm_wrist_pitch"),
+        pybind11::arg("arm_wrist_roll"),
+        pybind11::arg("hand"),
+        pybind11::arg("head_yaw"),
+        pybind11::arg("head_pitch"),
+        pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
+    .def("moveJointDeg", &JointController::moveJointDeg, "moveJointDeg",
         pybind11::arg("joint_num"),
         pybind11::arg("rad"),
         pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
-    .def("moveJointDeg", &JointController::moveJointDeg, "moveJoint",
+    .def("moveJointRad", &JointController::moveJointRad, "moveJointRad",
         pybind11::arg("joint_num"),
         pybind11::arg("rad"),
         pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
-    .def("moveHeadRad", &JointController::moveHeadRad, "move Head PanTilt",
-        pybind11::arg("pan_rad"),
-        pybind11::arg("tilt_rad"),
-        pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
-    .def("moveHeadDeg", &JointController::moveHeadDeg, "move Head PanTilt",
-        pybind11::arg("pan_rad"),
-        pybind11::arg("tilt_rad"),
-        pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
-    .def("moveArmRad", &JointController::moveArmRad, "move Arm",
-        pybind11::arg("arm_shoulder_pan"),
-        pybind11::arg("arm_shoulder_tilt"),
-        pybind11::arg("arm_elbow_tilt"),
-        pybind11::arg("arm_wrist_tilt"),
+    .def("moveArmDeg", &JointController::moveArmDeg, "moveArmDeg",
+        pybind11::arg("arm_shoulder_roll"),
+        pybind11::arg("arm_shoulder_pitch"),
+        pybind11::arg("arm_elbow_pitch"),
+        pybind11::arg("arm_forearm_roll"),
+        pybind11::arg("arm_wrist_pitch"),
+        pybind11::arg("arm_wrist_roll"),
         pybind11::arg("hand"),
         pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
-    .def("moveArmDeg", &JointController::moveArmDeg, "move Arm",
-        pybind11::arg("arm_shoulder_pan"),
-        pybind11::arg("arm_shoulder_tilt"),
-        pybind11::arg("arm_elbow_tilt"),
-        pybind11::arg("arm_wrist_tilt"),
+    .def("moveArmRad", &JointController::moveArmRad, "moveArmRad",
+        pybind11::arg("arm_shoulder_roll"),
+        pybind11::arg("arm_shoulder_pitch"),
+        pybind11::arg("arm_elbow_pitch"),
+        pybind11::arg("arm_forearm_roll"),
+        pybind11::arg("arm_wrist_pitch"),
+        pybind11::arg("arm_wrist_roll"),
         pybind11::arg("hand"),
+        pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
+    .def("moveHeadDeg", &JointController::moveHeadDeg, "moveHeadDeg",
+        pybind11::arg("head_yaw"),
+        pybind11::arg("head_pitch"),
+        pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
+    .def("moveHeadRad", &JointController::moveHeadRad, "moveHeadRad",
+        pybind11::arg("head_yaw"),
+        pybind11::arg("head_pitch"),
         pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
     .def("moveHandToTargetCoord", &JointController::moveHandToTargetCoord, "moveHandToTargetCoord",
         pybind11::arg("target_pos_x"), pybind11::arg("target_pos_y"), pybind11::arg("target_pos_z"),
-        pybind11::arg("shift_x"), pybind11::arg("shift_y"), pybind11::arg("shift_z"),
+        pybind11::arg("shift_x")     , pybind11::arg("shift_y")     , pybind11::arg("shift_z"),
         pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
     .def("moveHandToTargetTF", &JointController::moveHandToTargetTF, "moveHandToTargetTF",
         pybind11::arg("target_name"),
         pybind11::arg("shift_x"), pybind11::arg("shift_y"), pybind11::arg("shift_z"),
         pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
-    .def("moveHandToPlaceCoord", &JointController::moveHandToPlaceCoord, "move Hand To Placeable Position Coordinate",
+    .def("moveHandToPlaceCoord", &JointController::moveHandToPlaceCoord, "moveHandToPlaceCoord",
         pybind11::arg("target_pos_x"), pybind11::arg("target_pos_y"), pybind11::arg("target_pos_z"),
-        pybind11::arg("shift_x"), pybind11::arg("shift_y"), pybind11::arg("shift_z"),
+        pybind11::arg("shift_x")     , pybind11::arg("shift_y")     , pybind11::arg("shift_z"),
         pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
-    .def("moveHandToPlaceTF", &JointController::moveHandToPlaceTF, "move Hand To Placeable Position TF",
+    .def("moveHandToPlaceTF", &JointController::moveHandToPlaceTF, "moveHandToPlaceTF",
         pybind11::arg("target_name"),
         pybind11::arg("shift_x"), pybind11::arg("shift_y"), pybind11::arg("shift_z"),
         pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
-    .def("graspDecision", &JointController::graspDecision, "grasp Decision",
+    .def("moveHeadToTargetCoord", &JointController::moveHeadToTargetCoord, "moveHeadToTargetCoord",
+        pybind11::arg("target_pos_x"), pybind11::arg("target_pos_y"), pybind11::arg("target_pos_z"),
+        pybind11::arg("shift_x")     , pybind11::arg("shift_y")     , pybind11::arg("shift_z"),
+        pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
+    .def("moveHeadToTargetTF", &JointController::moveHeadToTargetTF, "moveHeadToTargetTF",
+        pybind11::arg("target_name"),
+        pybind11::arg("shift_x"), pybind11::arg("shift_y"), pybind11::arg("shift_z"),
+        pybind11::arg("sec") = 5.0, pybind11::arg("is_sleep") = true)
+    .def("graspDecision", &JointController::graspDecision, "graspDecision",
         pybind11::arg("min_curr") = 300,
         pybind11::arg("max_curr") = 1000)
-    .def("placeDecision", &JointController::placeDecision, "place Decision",
+    .def("placeDecision", &JointController::placeDecision, "placeDecision",
         pybind11::arg("min_curr") = 500,
         pybind11::arg("max_curr") = 1000);
 
   pybind11::class_<WheelController>(m, "WheelController")
     .def(pybind11::init<const std::string&>())
-    .def("controlWheelLinear", &WheelController::controlWheelLinear, "control Wheel Linear",
+    .def("controlWheelLinear", &WheelController::controlWheelLinear, "controlWheelLinear",
         pybind11::arg("distance"))
-    .def("controlWheelRotateRad", &WheelController::controlWheelRotateRad, "control Wheel Rotate Rad",
+    .def("controlWheelRotateRad", &WheelController::controlWheelRotateRad, "controlWheelRotateRad",
         pybind11::arg("angle_rad"))
-    .def("controlWheelRotateDeg", &WheelController::controlWheelRotateDeg, "control Wheel Rotate Deg",
+    .def("controlWheelRotateDeg", &WheelController::controlWheelRotateDeg, "controlWheelRotateDeg",
         pybind11::arg("angle_deg"))
-    .def("rad2Deg", &WheelController::rad2Deg, "rad 2 Deg",
+    .def("rad2Deg", &WheelController::rad2Deg, "rad2Deg",
         pybind11::arg("rad"))
-    .def("deg2Rad", &WheelController::deg2Rad, "deg 2 Rad",
+    .def("deg2Rad", &WheelController::deg2Rad, "deg2Rad",
         pybind11::arg("deg"));
 
-  pybind11::class_<sobit_light::WheelController, std::shared_ptr<sobit_light::WheelController>>(m, "WheelController")
-    .def(pybind11::init<const std::string&>())
-    .def("controlWheelLinear", &sobit_light::WheelController::controlWheelLinear, "Control Wheel Linear",
-        pybind11::arg("distance"))
-    .def("controlWheelRotateRad", &sobit_light::WheelController::controlWheelRotateRad, "Control Wheel Rotate Rad",
-        pybind11::arg("angle_rad"))
-    .def("controlWheelRotateDeg", &sobit_light::WheelController::controlWheelRotateDeg, "Control Wheel Rotate Deg",
-        pybind11::arg("angle_deg"))
-    .def("rad2Deg", &sobit_light::WheelController::rad2Deg, "Convert Radians to Degrees",
-        pybind11::arg("rad"))
-    .def("deg2Rad", &sobit_light::WheelController::deg2Rad, "Convert Degrees to Radians",
-        pybind11::arg("deg"));
-}
+//   pybind11::class_<sobit_light::WheelController, std::shared_ptr<sobit_light::WheelController>>(m, "WheelController")
+//     .def(pybind11::init<const std::string&>())
+//     .def("controlWheelLinear", &sobit_light::WheelController::controlWheelLinear, "controlWheelLinear",
+//         pybind11::arg("distance"))
+//     .def("controlWheelRotateRad", &sobit_light::WheelController::controlWheelRotateRad, "controlWheelRotateRad",
+//         pybind11::arg("angle_rad"))
+//     .def("controlWheelRotateDeg", &sobit_light::WheelController::controlWheelRotateDeg, "controlWheelRotateDeg",
+//         pybind11::arg("angle_deg"))
+//     .def("rad2Deg", &sobit_light::WheelController::rad2Deg, "rad2Deg",
+//         pybind11::arg("rad"))
+//     .def("deg2Rad", &sobit_light::WheelController::deg2Rad, "deg2Rad",
+//         pybind11::arg("deg"));
+// }
 
 }  // namespace sobit_light
