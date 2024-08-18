a65bceb16aa1d158c80d9ee62b293a91b63bf313
MrKeith99 <kvalentincardenas@hotmail.com>
2024-08-18 15:17:29 +0900

    [FIX] indentation

--- sobit_light_library/include/sobit_light_library/sobit_light_library.h
+++ sobit_light_library/include/sobit_light_library/sobit_light_library.h
@@ -1,20 +1,20 @@
-#ifndef _SOBIT_LIGHT_LIBRARY_SOBIT_LIGHT_LIBRARY_H_
-#define _SOBIT_LIGHT_LIBRARY_SOBIT_LIGHT_LIBRARY_H_
+#ifndef SOBIT_LIGHT_LIBRARY_H_
+#define SOBIT_LIGHT_LIBRARY_H_
 
 #include <pybind11/pybind11.h>
 #include <rclcpp/rclcpp.hpp>
 
 class ROSCommonNode : public rclcpp::Node {
-    public:
-        ROSCommonNode(const std::string& name)
-        : rclcpp::Node(name) {
-            // ROS2では特別な初期化は必要ありません
-        }
+ public:
+  ROSCommonNode(const std::string& name)
+  : rclcpp::Node(name) {
+    // ROS2では特別な初期化は必要ありません
+  }
 
-        ROSCommonNode()
-        : rclcpp::Node("sobit_light_library_node") {
-            // デフォルトのノード名で初期化
-        }
+  ROSCommonNode()
+  : rclcpp::Node("sobit_light_library_node") {
+    // デフォルトのノード名で初期化
+  }
 };
 
-#endif /* _SOBIT_LIGHT_LIBRARY_SOBIT_LIGHT_LIBRARY_H_ */
+#endif /* SOBIT_LIGHT_LIBRARY_H_ */
