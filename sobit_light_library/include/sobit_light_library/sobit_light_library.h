379b6f7917969bea043ebaba1c2992910e298659
MrKeith99 <kvalentincardenas@hotmail.com>
2024-08-18 18:47:29 +0900

    [UPD] new node generation

--- sobit_light_library/include/sobit_light_library/sobit_light_library.h
+++ sobit_light_library/include/sobit_light_library/sobit_light_library.h
@@ -1,20 +1,21 @@
 #ifndef SOBIT_LIGHT_LIBRARY_H_
 #define SOBIT_LIGHT_LIBRARY_H_
 
-#include <pybind11/pybind11.h>
+// #include <pybind11/pybind11.h>
 #include <rclcpp/rclcpp.hpp>
 
 class ROSCommonNode : public rclcpp::Node {
  public:
-  ROSCommonNode(const std::string& name)
-  : rclcpp::Node(name) {
-    // ROS2では特別な初期化は必要ありません
-  }
-
   ROSCommonNode()
-  : rclcpp::Node("sobit_light_library_node") {
-    // デフォルトのノード名で初期化
-  }
+  : rclcpp::Node("sobit_light_library_node") {}
+  ROSCommonNode(const std::string& name)
+  : rclcpp::Node(name) {}
+  ROSCommonNode(const rclcpp::NodeOptions& options)
+  : rclcpp::Node("sobit_light_library_node", options) {}
+  ROSCommonNode(
+      const std::string& name,
+      const rclcpp::NodeOptions& options)
+  : rclcpp::Node(name, options) {}
 };
 
 #endif  // SOBIT_LIGHT_LIBRARY_H_
