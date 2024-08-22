e1b4e3a7efd77a942b3cce30f22a55374da6a628
MrKeith99 <kvalentincardenas@hotmail.com>
2024-08-22 23:33:42 +0900

    [ADD] add invKin for python

--- sobit_light_library_python/sobit_light_library_python/sobit_light_joint_controller.py
+++ sobit_light_library_python/sobit_light_library_python/sobit_light_joint_controller.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from math import (radians, degrees, fmod, pi)
-from typing import Callable
+from math import (sin, cos, acos, atan2, sqrt, pow, pi)
 
-from rcl_interfaces.msg import SetParametersResult
 import rclpy
 from rclpy.node import Node
 
@@ -16,16 +14,19 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from geometry_msgs.msg import Point, TransformStamped
 from sobits_msgs.msg import CurrentStateArray, CurrentState
 
+from sobit_light_wheel_controller import WheelController
+
 class Joints(enumerate):
-  kArmShoulderRollJoint = 0
+  kArmShoulderRollJoint  = 0
   kArmShoulderPitchJoint = 1
-  kArmElbowPitchJoint = 2
-  kArmWristPitchJoint = 3
-  kArmWristRollJoint = 4
-  kHandJoint = 5
-  kHeadYawJoint = 6
-  kHeadPitchJoint = 7
-  kJointNum = 8
+  kArmElbowPitchJoint    = 2
+  kArmForearmRollJoint   = 3
+  kArmWristPitchJoint    = 4
+  kArmWristRollJoint     = 5
+  kHandJoint             = 6
+  kHeadYawJoint          = 7
+  kHeadPitchJoint        = 8
+  kJointNum              = 9
 
 class Pose:
   def __init__(self, pose_name, joint_val):
@@ -34,11 +35,12 @@ class Pose:
 
 
 kJointNames = [
-    'arm_shoulder_roll_joint', 
-    'arm_shoulder_pitch_joint', 
-    'arm_elbow_pitch_joint', 
-    'arm_wrist_pitch_joint', 
-    'arm_wrist_roll_joint', 
+    'arm_shoulder_roll_joint',
+    'arm_shoulder_pitch_joint',
+    'arm_elbow_pitch_joint',
+    'arm_forearm_roll_joint',
+    'arm_wrist_pitch_joint',
+    'arm_wrist_roll_joint',
     'hand_joint',
     'head_yaw_joint',
     'head_pitch_joint',
@@ -81,7 +83,17 @@ class JointController(Node):
     # Variables
     self.tf_buffer_ = Buffer()
     self.tf_listener_ = TransformListener(self.tf_buffer_, self)
+
+    # Constants
     self.kCurrent = [0.0] * Joints.kJointNum
+
+    self.kArmUpper   = 0.128
+    self.kArmUpperX  = 0.022
+    self.kArmLower   = 0.124
+    self.kArmGripper = 0.064 + 0.11225
+    self.kArmLength  = self.kArmUpper + self.kArmLower
+    self.kGraspMinZ  = 0.035
+    self.kGraspMaxZ  = 0.080
   
 
     # Rate
@@ -149,17 +161,18 @@ class JointController(Node):
     head_joint_trajectory = JointTrajectory()
 
     # Arm joints
-    self.setJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmShoulderRollJoint], arm_joint_vals[0], duration)
+    self.setJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmShoulderRollJoint] , arm_joint_vals[0], duration)
     self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmShoulderPitchJoint], arm_joint_vals[1], duration)
-    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmElbowPitchJoint], arm_joint_vals[2], duration)
-    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmWristPitchJoint], arm_joint_vals[3], duration)
-    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmWristRollJoint], arm_joint_vals[4], duration)
+    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmElbowPitchJoint]   , arm_joint_vals[2], duration)
+    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmForearmRollJoint]  , arm_joint_vals[3], duration)
+    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmWristPitchJoint]   , arm_joint_vals[4], duration)
+    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmWristRollJoint]    , arm_joint_vals[5], duration)
 
     # Hand joint
     self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kHandJoint], hand_joint_val, duration)
 
     # Head joints
-    self.addJointTrajectoryPoint(head_joint_trajectory, kJointNames[Joints.kHeadYawJoint], head_joint_vals[0], duration)
+    self.addJointTrajectoryPoint(head_joint_trajectory, kJointNames[Joints.kHeadYawJoint]  , head_joint_vals[0], duration)
     self.addJointTrajectoryPoint(head_joint_trajectory, kJointNames[Joints.kHeadPitchJoint], head_joint_vals[1], duration)
 
     # Check publishers connection
@@ -190,18 +203,20 @@ class JointController(Node):
 
   def moveArmRad(self,
       arm_joint_vals,
+      hand_joint_val,
       duration=1.0, wait=True):
     arm_joint_trajectory = JointTrajectory()
 
     # Arm joints
-    self.setJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmShoulderRollJoint], arm_joint_vals[0], duration)
+    self.setJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmShoulderRollJoint] , arm_joint_vals[0], duration)
     self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmShoulderPitchJoint], arm_joint_vals[1], duration)
-    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmElbowPitchJoint], arm_joint_vals[2], duration)
-    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmWristPitchJoint], arm_joint_vals[3], duration)
-    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmWristRollJoint], arm_joint_vals[4], duration)
+    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmElbowPitchJoint]   , arm_joint_vals[2], duration)
+    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmForearmRollJoint]  , arm_joint_vals[3], duration)
+    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmWristPitchJoint]   , arm_joint_vals[4], duration)
+    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kArmWristRollJoint]    , arm_joint_vals[5], duration)
 
     # Hand joint
-    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kHandJoint], arm_joint_vals[5], duration)
+    self.addJointTrajectoryPoint(arm_joint_trajectory, kJointNames[Joints.kHandJoint], hand_joint_val, duration)
     
     # Check publishers connection
     self.checkPublishersConnection(self.pub_arm_control_)
@@ -216,7 +231,7 @@ class JointController(Node):
     head_joint_trajectory = JointTrajectory()
 
     # Head joints
-    self.setJointTrajectoryPoint(head_joint_trajectory, kJointNames[Joints.kHeadYawJoint], head_joint_vals[0], duration)
+    self.setJointTrajectoryPoint(head_joint_trajectory, kJointNames[Joints.kHeadYawJoint]  , head_joint_vals[0], duration)
     self.addJointTrajectoryPoint(head_joint_trajectory, kJointNames[Joints.kHeadPitchJoint], head_joint_vals[1], duration)
 
     # Check publishers connection
@@ -230,25 +245,152 @@ class JointController(Node):
       target_coord,
       shift_coord,
       duration=1.0, wait=True):
-    pass
+    is_reached = False
+    wheel_ctrl = WheelController()
+
+    goal_coord = [target_coord[0] + shift_coord[0],
+                  target_coord[1] + shift_coord[1],
+                  target_coord[2] + shift_coord[2]]
+    
+    if (goal_coord[2] > self.kArmLenght*2):
+      self.get_logger().error('The target is too far (%f > %f)' % (goal_coord[2], self.kArmLenght*2))
+      return is_reached
+    
+    elif (goal_coord[2] < -self.kArmLenght):
+      self.get_logger().error('The target is located too low (%f < %f)' % (goal_coord[2], -self.kArmLenght))
+      return is_reached
+    
+    arm_shoulder_roll_joint_rad  = 0.0
+    arm_shoulder_pitch_joint_rad = 0.0
+    arm_elbow_pitch_joint_rad    = 0.0
+    arm_forearm_roll_joint_rad   = 0.0
+    arm_wrist_pitch_joint_rad    = 0.0
+    arm_wrist_roll_joint_rad     = 0.0
+    hand_joint_rad               = 0.0
+    elbow_joint_z                = 0.0
+
+    if (self.kArmUpper < goal_coord[2]):
+      self.get_logger().info('The target is located above the arm (%f > %f)' % (goal_coord[2], self.kArmUpper))
+      elbow_joint_z = self.kArmLength
+      arm_shoulder_pitch_joint_rad = pi/2.
+
+    elif (0 <= goal_coord[2] and goal_coord[2] <= self.kArmUpper):
+      self.get_logger().info('The target is located below arm_elbow_pitch_join and above shoulder_pitch_joint (%f <= %f <= %f)' % (0, goal_coord[2], self.kArmUpper))
+      elbow_joint_z = self.kArmLength * sin(pi/4.)
+      arm_shoulder_pitch_joint_rad = pi/4.
+
+    elif (goal_coord[2] < 0):
+      self.get_logger().info('The target is located below the shoulder_pitch_joint (%f < %f)' % (goal_coord[2], 0))
+      elbow_joint_z = 0.0
+      arm_shoulder_pitch_joint_rad = 0.0
+
+    base_z = elbow_joint_z - goal_coord[1]
+    arm_elbow_pitch_joint_rad = acos(base_z / self.kArmUpper)
+    arm_wrist_pitch_joint_rad = pi/2 - (arm_shoulder_pitch_joint_rad + arm_elbow_pitch_joint_rad)
+
+    arm_wrist_joint_x = self.kArmUpper * (cos(arm_shoulder_pitch_joint_rad) + cos(arm_shoulder_pitch_joint_rad + arm_elbow_pitch_joint_rad))
+    arm_wrist_joint_z = self.kArmUpper * (sin(arm_shoulder_pitch_joint_rad) + sin(arm_shoulder_pitch_joint_rad + arm_elbow_pitch_joint_rad))
+    
+    # Rotate the robot
+    rot_rad = atan2(goal_coord[1], goal_coord[0])
+    wheel_ctrl.controlWheelRotateRad(rot_rad)
+
+    # Move forward
+    linear_m = sqrt(pow(goal_coord[0], 2) + pow(goal_coord[1], 2))
+    wheel_ctrl.controlWheelLinear(linear_m)
+
+    is_reached = self.moveArmRad(
+      [
+      arm_shoulder_roll_joint_rad,
+      arm_shoulder_pitch_joint_rad,
+      arm_elbow_pitch_joint_rad,
+      arm_forearm_roll_joint_rad,
+      arm_wrist_pitch_joint_rad,
+      arm_wrist_roll_joint_rad
+      ],
+      hand_joint_rad,
+      duration, wait)
+    
+    return is_reached
+
 
   def moveHandToTargetTF(self,
       target_name,
       shift_coord,
       duration=1.0, wait=True):
-    pass
+    is_reached = False
+
+    transformStamped = TransformStamped()
+
+    try:
+      self.tf_buffer_.can_transform(
+          'arm_base_joint', target_name,
+          rclpy.time.Time(), rclpy.time.Duration(seconds=1.0))
+      transformStamped = self.tf_buffer_.lookup_transform('arm_base_joint', target_name, rclpy.time.Time())
+    except TransformException as e:
+      self.get_logger().error('Failed to get transform: %s' % e)
+      return is_reached
+    
+    target_coord = [
+        transformStamped.transform.translation.x,
+        transformStamped.transform.translation.y,
+        transformStamped.transform.translation.z]
+    is_reached = self.moveHandToTargetCoord(
+        target_coord,
+        shift_coord,
+        duration, wait)
+
+    return is_reached
+
 
   def moveHandToPlaceCoord(self,
       target_coord,
       shift_coord,
       duration=1.0, wait=True):
-    pass
+    is_placed = False
+
+    while not(is_placed and self.graspDecision(500.0, 1000.0)):
+      is_placed = self.moveHandToTargetCoord(
+          target_coord,
+          shift_coord,
+          duration, wait)
+      
+      if not is_placed:
+        self.get_logger().error('Failed to place the object')
+        return is_placed
+      
+      shift_coord[2] = -0.002
+    
+    return is_placed
+
 
   def moveHandToPlaceTF(self,
       target_name,
       shift_coord,
       duration=1.0, wait=True):
-    pass
+    is_placed = False
+
+    transformStamped = TransformStamped()
+
+    try:
+      self.tf_buffer_.can_transform(
+          'arm_base_joint', target_name,
+          rclpy.time.Time(), rclpy.time.Duration(seconds=1.0))
+      transformStamped = self.tf_buffer_.lookup_transform('arm_base_joint', target_name, rclpy.time.Time())
+    except TransformException as e:
+      self.get_logger().error('Failed to get transform: %s' % e)
+      return is_placed
+    
+    target_coord = [
+        transformStamped.transform.translation.x,
+        transformStamped.transform.translation.y,
+        transformStamped.transform.translation.z]
+    is_placed = self.moveHandToPlaceCoord(
+        target_coord,
+        shift_coord,
+        duration, wait)
+
+    return is_placed
 
   def graspDecision(self,
       min_current, max_current):
