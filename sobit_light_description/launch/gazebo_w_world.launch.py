060be3b9d410e8eec87d931c76b45a48c768db94
MrKeith99 <kvalentincardenas@hotmail.com>
2024-12-16 18:11:30 +0900

    [ADD] pub kachaka camera

--- sobit_light_description/launch/gazebo_w_world.launch.py
+++ sobit_light_description/launch/gazebo_w_world.launch.py
@@ -18,10 +18,10 @@ def generate_launch_description():
     # Launch Arguments
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
 
-    ignition_ros2_control_demos_path = os.path.join(
+    gz_ros2_control_demos_path = os.path.join(
         get_package_share_directory('sobit_light_description'),)
 
-    xacro_file = os.path.join(ignition_ros2_control_demos_path,
+    xacro_file = os.path.join(gz_ros2_control_demos_path,
                               'robots',
                               'sobit_light_robot.urdf.xacro')
 
@@ -38,7 +38,7 @@ def generate_launch_description():
         parameters=[params],
     )
 
-    ignition_spawn_entity = Node(
+    gz_spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
         output='screen',
@@ -130,7 +130,7 @@ def generate_launch_description():
             launch_arguments=[('gz_args', world_file_path)]),
         RegisterEventHandler(
             event_handler=OnProcessExit(
-                target_action=ignition_spawn_entity,
+                target_action=gz_spawn_entity,
                 on_exit=[load_joint_state_controller],
             )
         ),
@@ -159,7 +159,7 @@ def generate_launch_description():
             )
         ),
         node_robot_state_publisher,
-        ignition_spawn_entity,
+        gz_spawn_entity,
         # Launch Arguments
         DeclareLaunchArgument(
             'use_sim_time',
