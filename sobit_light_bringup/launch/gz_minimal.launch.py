da1f3163d40ccffe6bb433056d5883311b352c81
MrKeith99 <kvalentincardenas@hotmail.com>
2025-01-15 21:30:43 +0900

    [FIX] indent

--- sobit_light_bringup/launch/gz_minimal.launch.py
+++ sobit_light_bringup/launch/gz_minimal.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
             FindPackageShare('sobit_light_bringup'),
             'rviz',
             'gazebo.rviz'
-        ])
+    ])
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
