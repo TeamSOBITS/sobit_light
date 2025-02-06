0dd4c88287d7cbab882842f854a2976a5df1a238
MrKeith99 <kvalentincardenas@hotmail.com>
2025-02-06 21:16:35 +0900

    [UPD] load poses

--- sobit_light_library/launch/action_server.launch.py
+++ sobit_light_library/launch/action_server.launch.py
@@ -1,3 +1,6 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -6,11 +9,18 @@ def generate_launch_description():
     robot_name = "sobit_light"
     robot_id = 0
 
+    pose_config = os.path.join(
+        get_package_share_directory("sobit_light_library"),
+        "config",
+        "pose_list.yaml",
+    )
+
     joint_action_server_node = Node(
         package="sobit_light_library",
         executable="joint_action_server",
         name="joint_action_server",
         namespace=robot_name if robot_id == 0 else f"{robot_name}_{robot_id}",
+        parameters=[pose_config],
         output="screen",
     )
 
