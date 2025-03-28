ffae3190d3dafa85c4951beb829e25d41d040112
MrKeith99 <kvalentincardenas@hotmail.com>
2025-03-28 20:40:32 +0900

    [FIX] tf msg

--- sobit_light_bringup/launch/gz_minimal.launch.py
+++ sobit_light_bringup/launch/gz_minimal.launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
         executable='parameter_bridge',
         arguments=[
                     "/clock" + "@rosgraph_msgs/msg/Clock" + "[ignition.msgs.Clock",
-                    "/tf" + "@tf2_msgs/msg/TFMessage" + "[ignition.msgs.TFMessage",
+                    "/tf" + "@tf2_msgs/msg/TFMessage" + "[ignition.msgs.Pose_V",
                    ],
         output='screen'
     )
