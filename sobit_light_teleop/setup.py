c37780916493c927da5f1baf4137f9b5e0a193c9
satokeeen <kenta.sato5044@gmail.com>
2025-06-21 05:41:54 +0900

    add dualshock_teleop launch

--- sobit_light_teleop/setup.py
+++ sobit_light_teleop/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+
 
 package_name = 'sobit_light_teleop'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', 
+            glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +28,7 @@ setup(
             'keyboard_all_teleop = sobit_light_teleop.keyboard_all_teleop:main',
             'vr_teleop_1 = sobit_light_teleop.vr_teleop_1:main',
             'vr_teleop_2 = sobit_light_teleop.vr_teleop_2:main',
+            'dualshock_teleop = sobit_light_teleop.dualshock_teleop:main'
         ],
     },
 )
