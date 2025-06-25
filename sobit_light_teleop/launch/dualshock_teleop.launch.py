dc5ed6d5fcdc6f777efa4cc55642b305752ee081
MrKeith99 <kvalentincardenas@hotmail.com>
2025-06-25 19:15:50 +0900

    [UPD] robot name param

--- sobit_light_teleop/launch/dualshock_teleop.launch.py
+++ sobit_light_teleop/launch/dualshock_teleop.launch.py
@@ -1,19 +1,31 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
+    arg_robot_name = DeclareLaunchArgument('robot_name', default_value='sobit_light')
+
     return LaunchDescription([
+        arg_robot_name,
+        OpaqueFunction(function = launch_gz),
+    ])
+
+
+def launch_gz(context, *args, **kwargs):
+    robot_name = LaunchConfiguration('robot_name').perform(context)
+
+    return [
         Node(
             package='sobit_light_teleop',
             executable='dualshock_teleop',
-            name='dualshock_teleop'
+            name='dualshock_teleop',
+            namespace=robot_name,
         ),
         Node(
             package='joy_linux',
             executable='joy_linux_node',
             name='joy_linux_node',
-            remappings=[
-                ('/joy', '/sobit_light/joy')
-            ]
+            namespace=robot_name,
         )
-    ])
+    ]
