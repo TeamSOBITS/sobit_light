901521057d2c77a4ec0585820e4bfd6b1489d33c
MrKeith99 <kvalentincardenas@hotmail.com>
2025-06-25 22:45:32 +0900

    [TODO] QoS setup

--- sobit_light_library/src/sobit_light_joint_action_server.cpp
+++ sobit_light_library/src/sobit_light_joint_action_server.cpp
@@ -9,9 +9,9 @@ JointActionServer::JointActionServer(const rclcpp::NodeOptions & options = rclcp
 {
   // Configure the QoS profile
   rclcpp::QoS qos_profile(1); // depth = 1
-  qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
-  qos_profile.history(RMW_QOS_POLICY_HISTORY_KEEP_LAST);
-  qos_profile.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
+  // qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
+  // qos_profile.history(RMW_QOS_POLICY_HISTORY_KEEP_LAST);
+  // qos_profile.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
 
 
   this->action_server_move_joints_ = rclcpp_action::create_server<MoveJoint>(
