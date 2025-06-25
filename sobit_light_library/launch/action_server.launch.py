7023dd74e1e9b3df7bd63ccf69aaeec37e5aa745
MrKeith99 <kvalentincardenas@hotmail.com>
2025-06-25 22:45:07 +0900

    [FIX] add simulation handler (allows to play action in sim)

--- sobit_light_library/launch/action_server.launch.py
+++ sobit_light_library/launch/action_server.launch.py
@@ -8,15 +8,18 @@ from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     arg_robot_name = DeclareLaunchArgument('robot_name', default_value='sobit_light')
+    arg_enable_gz = DeclareLaunchArgument('enable_gz', default_value='True')
 
     return LaunchDescription([
         arg_robot_name,
+        arg_enable_gz,
         OpaqueFunction(function = launch_gz),
     ])
 
 
 def launch_gz(context, *args, **kwargs):
     robot_name = LaunchConfiguration('robot_name').perform(context)
+    enable_gz = LaunchConfiguration('enable_gz').perform(context)
 
     pose_config = os.path.join(
         get_package_share_directory("sobit_light_library"),
@@ -29,7 +32,9 @@ def launch_gz(context, *args, **kwargs):
         executable="joint_action_server",
         name="joint_action_server",
         namespace=robot_name,
-        parameters=[pose_config],
+        parameters=[pose_config,
+            {"use_sim_time": True if enable_gz == 'True' else False},
+        ],
         output="screen",
     )
 
@@ -38,6 +43,9 @@ def launch_gz(context, *args, **kwargs):
         executable="wheel_action_server",
         name="wheel_action_server",
         namespace=robot_name,
+        parameters=[
+            {"use_sim_time": True if enable_gz == 'True' else False},
+        ],
         output="screen",
     )
 
