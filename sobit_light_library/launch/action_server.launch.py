974114c9262a7af7eec4316d670f1d615d8e6d69
MrKeith99 <kvalentincardenas@hotmail.com>
2025-06-25 19:14:06 +0900

    [UPD] allow to be imported

--- sobit_light_library/launch/action_server.launch.py
+++ sobit_light_library/launch/action_server.launch.py
@@ -3,11 +3,20 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
-    robot_name = "sobit_light"
-    robot_id = 0
+    arg_robot_name = DeclareLaunchArgument('robot_name', default_value='sobit_light')
+
+    return LaunchDescription([
+        arg_robot_name,
+        OpaqueFunction(function = launch_gz),
+    ])
+
+
+def launch_gz(context, *args, **kwargs):
+    robot_name = LaunchConfiguration('robot_name').perform(context)
 
     pose_config = os.path.join(
         get_package_share_directory("sobit_light_library"),
@@ -19,7 +28,7 @@ def generate_launch_description():
         package="sobit_light_library",
         executable="joint_action_server",
         name="joint_action_server",
-        namespace=robot_name if robot_id == 0 else f"{robot_name}_{robot_id}",
+        namespace=robot_name,
         parameters=[pose_config],
         output="screen",
     )
@@ -28,12 +37,12 @@ def generate_launch_description():
         package="sobit_light_library",
         executable="wheel_action_server",
         name="wheel_action_server",
-        namespace=robot_name if robot_id == 0 else f"{robot_name}_{robot_id}",
+        namespace=robot_name,
         output="screen",
     )
 
 
-    return LaunchDescription([
+    return [
         joint_action_server_node,
         wheel_action_server_node,
-    ])
+    ]
