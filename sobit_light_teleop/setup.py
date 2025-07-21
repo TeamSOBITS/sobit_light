15dc6736e62de4feb426998a6e2b5a9b7e7ccac0
MrKeith99 <kvalentincardenas@hotmail.com>
2025-07-21 15:58:15 +0900

    [UPD] package.xml update metainfo

--- sobit_light_teleop/setup.py
+++ sobit_light_teleop/setup.py
@@ -6,7 +6,7 @@ package_name = 'sobit_light_teleop'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='2.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -17,10 +17,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='sobits',
+    maintainer='Keith Valentin',
     maintainer_email='kvalentincardenas@hotmail.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
+    description='SOBIT LIGHT teleoperation systems',
+    license='BSD-3-Clause',
     entry_points={
         'console_scripts': [
             'keyboard_nav_teleop = sobit_light_teleop.keyboard_nav_teleop:main',
